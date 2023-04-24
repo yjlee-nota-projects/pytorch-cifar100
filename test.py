8858ded044a4e24eb90b15552612cae4d220a91a
Yuje Lee <yuje.lee@nota.ai>
2023-04-24 01:42:10 +0000

    fix: delete codes related to args.weights We export models as fx.GraphModule format when we train them, so args.weights is not required now.

--- test.py
+++ test.py
@@ -23,7 +23,6 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-net', type=str, required=True, help='net type')
-    parser.add_argument('-weights', type=str, help='the weights file you want to test')
     parser.add_argument('-gpu', action='store_true', default=False, help='use gpu or not')
     parser.add_argument('-b', type=int, default=256, help='batch size for dataloader')
     args = parser.parse_args()
@@ -37,8 +36,7 @@ if __name__ == '__main__':
         num_workers=4,
         batch_size=args.b,
     )
-    if args.weights is not None:
-       net.load_state_dict(torch.load(args.weights))
+    
     print(net)
     net.eval()
 
